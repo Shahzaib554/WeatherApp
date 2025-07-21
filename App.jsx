a77adc8da79a126617239f56b1bfc358a4d3c09f
Shahzaib554 <86713644+Shahzaib554@users.noreply.github.com>
2025-07-21 13:40:19 -0700

    Css Code Implememted!

--- App.jsx
+++ App.jsx
@@ -1,22 +1,62 @@
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import {
+  View,
+  Image,
+  StyleSheet,
+  Dimensions,
+  TextInput,
+  Text,
+} from 'react-native';
+
+const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
 
 function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello</Text>
+      {/* Background Image */}
+
+      <Image
+        source={require('./images/weather.png')}
+        style={styles.backgroundImage}
+        resizeMode="cover"
+      />
+      {/* Text Input */}
+      <TextInput
+        placeholder="Enter City"
+        placeholderTextColor="#888"
+        style={styles.textInput}
+      />
+      <Text style={styles.text}>0 C°</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#fff',
     flex: 1,
-    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  backgroundImage: {
+    alignItems: 'stretch',
+    width: deviceWidth,
+    height: deviceHeight,
+  },
+  textInput: {
+    color: '#000000',
+    position: 'absolute',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    width: 250,
+    margin: 10,
   },
   text: {
-    fontSize: 20,
-    padding: 10,
-    color: 'black',
+    color: '#FFFF00',
+    position: 'absolute',
+    fontSize: 30,
+    marginTop: 200,
   },
 });
 
