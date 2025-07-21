5dd321c960a9a503b677320a50138a800deb2abf
Shahzaib554 <86713644+Shahzaib554@users.noreply.github.com>
2025-07-21 12:36:11 -0700

    Resolved the Issues from my code!

--- App.jsx
+++ App.jsx
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 function App() {
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 20,
+    padding: 10,
+    color: 'black',
+  },
+});
 
 export default App;
